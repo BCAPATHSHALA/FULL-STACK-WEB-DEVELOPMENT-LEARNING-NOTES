08447c4a3e515a01bbc8cbd0860a088b53debc1b
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2023-04-19 14:55:16 +0530

    initial commit

---
+++ 1. Master ReactJS with 5 Projects/third/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Coins from "./components/Coins";
+import Exchanges from "./components/Exchanges";
+import CoinDetails from "./components/CoinDetails";
+import Footer from "./components/Footer"
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/coins" element={<Coins />} />
+          <Route path="/exchanges" element={<Exchanges />} />
+          <Route path="/coin/:id" element={<CoinDetails />} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
+  );
+};
+
+export default App;
