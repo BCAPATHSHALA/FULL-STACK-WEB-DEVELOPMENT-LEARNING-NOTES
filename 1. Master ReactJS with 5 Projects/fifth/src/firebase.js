d2a544b40b8012c9d27a7964db3677c8400ce492
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2023-04-22 17:13:34 +0530

    initial commit

--- 1. Master ReactJS with 5 Projects/fifth/src/firebase.js
+++ 1. Master ReactJS with 5 Projects/fifth/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "domain",
-  projectId: "projectID",
-  storageBucket: "bucket",
-  messagingSenderId: "senderID",
-  appId: "appID",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 export const app = initializeApp(firebaseConfig);
