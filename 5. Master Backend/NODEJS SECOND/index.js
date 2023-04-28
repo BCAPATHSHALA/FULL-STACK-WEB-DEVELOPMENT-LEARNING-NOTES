2c86b2d217759373855b10a29d096b985276937b
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2023-04-28 12:58:29 +0530

    initial commit

--- 5. Master Backend/NODEJS SECOND/index.js
+++ 5. Master Backend/NODEJS SECOND/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
-
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 //config .env file
 dotenv.config({ path: "./.env" });
@@ -26,53 +28,100 @@ const database = () => {
 database();
 
 // Creating Mongoose Schema/Model
-const messageSchema = new mongoose.Schema({
-  name: String,
+const userSchema = new mongoose.Schema({
+  password: String,
   email: String,
+  name: String,
 });
 
-const Messge = new mongoose.model("Message", messageSchema);
+const User = new mongoose.model("User", userSchema);
 
 const app = express();
-const users = [];
 
 //! Using Middlewares
-// Setting Up Static Folder Public
 app.use(express.static(path.join(path.resolve(), "public")));
-// Access Data From HTML Form
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Setting Up View Engine
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index", { name: "MANOJ NISHAD" });
+const isAuthenticated = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const decoded = jwt.verify(token, "vhhhhg893ugfuefwt82rjkefuk");
+
+    req.user = await User.findById(decoded._id);
+
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
+// Routes
+app.get("/", isAuthenticated, (req, res, next) => {
+  res.render("logout");
 });
 
-app.get("/add", async (req, res) => {
-  await Messge.create({ name: "MANOJ NISHAD", email: "manojnishad@gmail.com" });
-  res.send("Nice");
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
-app.get("/success", (req, res) => {
-  res.render("success");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.post("/contact", async (req, res) => {
-  const { name, email } = req.body;
-  const data = await Messge.create({
-    name,
+// API's
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.redirect("/register");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch)
+    return res.render("login", { email, message: "Incorrect Password" });
+
+  const token = jwt.sign({ id: user._id }, "vhhhhg893ugfuefwt82rjkefuk");
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000),
+  });
+  res.redirect("/");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  let user = await User.findOne({ email });
+  if (user) {
+    return res.redirect("/login");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  user = await User.create({
+    password: hashedPassword,
     email,
+    name,
   });
-  // Saving Data In Database
-  await data.save();
-  res.redirect("/success");
+  await user.save();
+
+  const token = jwt.sign({ id: user._id }, "vhhhhg893ugfuefwt82rjkefuk");
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000),
+  });
+  res.redirect("/");
 });
 
-app.get("/users", (req, res) => {
-  res.json({
-    users,
+app.get("/logout", (req, res) => {
+  res.cookie("token", null, {
+    httpOnly: true,
+    expires: new Date(Date.now()),
   });
+  res.redirect("/");
 });
 
 app.listen(5000, () => {
