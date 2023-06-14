cf36552828a827165aad8c12fb63c1dca507ea1f
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2023-06-14 11:50:15 +0530

    initial commit

---
+++ 8. Rich Text Editor/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Editor from "./component/Editor";
+
+function App() {
+  return (
+    <>
+      <Editor />
+    </>
+  );
+}
+
+export default App;
---
+++ 8. Rich Text Editor/frontend/src/component/Editor.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+
+import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+
+const Component = styled.div`
+  background: #f5f5f5;
+`;
+
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"],
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ indent: "-1" }, { indent: "+1" }],
+  [{ direction: "rtl" }],
+  ["link", "image", "video"],
+
+  [{ size: ["small", false, "large", "huge"] }],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }],
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"],
+];
+
+const Editor = () => {
+  // Initialize the Quill Text Editor
+  useEffect(() => {
+    new Quill("#container", {
+      theme: "snow",
+      modules: { toolbar: toolbarOptions },
+    });
+  }, []);
+
+  return (
+    <Component>
+      <Box className="container" id="container"></Box>
+    </Component>
+  );
+};
+
+export default Editor;
